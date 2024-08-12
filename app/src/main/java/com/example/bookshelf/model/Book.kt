eaaac7cb31696166e4d082e1b703e9730a9a201a
YouMayknow <successfulkaran@gmail.com>
2024-08-12 18:24:33 +0530

    reiniltaizing the ui branch

--- app/src/main/java/com/example/bookshelf/model/Book.kt
+++ app/src/main/java/com/example/bookshelf/model/Book.kt
@@ -4,5 +4,15 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Book(
+    val items: List<BookItem>
+)
+
+@Serializable
+data class BookItem(
     val id : String
+)
+
+@Serializable
+data class BookImage(
+    val thumbnail : String
 )
